8ce39983286df2d192b495808120d0df126c5a63
marscom <marscompereira@gmail.com>
2025-07-26 17:44:37 -0500

    template strings

--- 2034-logica-programacion-1-ProyectoInicial/app.js
+++ 2034-logica-programacion-1-ProyectoInicial/app.js
@@ -4,7 +4,9 @@ let numeroUsuario = prompt("Me indicas un numero por favor:");
 console.log(numeroUsuario);
 
 if (numeroUsuario == numeroSecreto) {
-  console.log("Felicidades, acertaste el numero");
+  console.log(
+    `Felicidades, acertaste el numero , el numero es: ${numeroUsuario}`
+  );
 } else {
   console.log("perdiste");
 }
